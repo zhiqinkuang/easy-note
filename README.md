7a3986591bbc36d22ede8690de50072cbd89b53a
kiri <zhiqinkuang@gmail.com>
2025-03-25 16:29:01 +0800

    added readme

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Easy Note
 
-## Project description
+*  [原项目参考链接](https://github.com/cloudwego/biz-demo/tree/main/easy_note)
+## 项目描述
+* 一个用来学习微服务的小案例,实现 note 和用户功能
+* 使用的框架是 kitex + Hertz 
+* 系统框架是 etcd 用于服务发现
+* 具体的框架结构如图所示
 
+![系统框架](https://pic-bed-private.oss-cn-shanghai.aliyuncs.com/pic-bedeasy-note-arch.png)
+
+## 项目实现思路
+
+### 项目初始化
+* 创建docker 需要的环境
+* 建立idl文件夹,规定接口建立 note.thrift, user.thrift, api.thrift
+* 建立Makefile, 写入代码生成指令
--- idl/user.thrift
+++ idl/user.thrift
@@ -1,18 +1,20 @@
-namespace go demouser
+namespace  go  demouser
 
-enum ErrCode {
-    SuccessCode                = 0
-    ServiceErrCode             = 10001
-    ParamErrCode               = 10002
-    UserAlreadyExistErrCode    = 10003
-    AuthorizationFailedErrCode = 10004
+enum ErrCode{
+  SuccessCode  =0
+  ServiceErrCode  =10001
+  ParamErrCode    =10002
+  UserAlreadyExistCode =10003
+  AuthoriztionFailerdErrCode =10004
 }
 
-struct BaseResp {
-    1: i64 status_code
-    2: string status_message
-    3: i64 service_time
+
+struct BaseResp{
+   1: i64 status_code
+   2: string status_message
+   3: i64 service_time
 }
+
 
 struct User {
     1: i64 user_id
@@ -20,36 +22,37 @@ struct User {
     3: string avatar
 }
 
-struct CreateUserRequest {
-    1: string username (vt.min_size = "1")
-    2: string password (vt.min_size = "1")
+struct CreateUserRequest{
+1: string username (vt.min_size = "1")
+2: string password (vt.min_size = "1")
 }
 
-struct CreateUserResponse {
-    1: BaseResp base_resp
+struct CreateUserResponse{
+1: BaseResp base_resp
 }
 
-struct MGetUserRequest {
-    1: list<i64> user_ids (vt.min_size = "1")
+struct MGetUserRequest{
+1: list<i64> user_ids (vt.min_size ="1")
 }
 
-struct MGetUserResponse {
-    1: list<User> users
-    2: BaseResp base_resp
+struct MGetUserResponse{
+1: list<User> users (vt.min_size ="1")
+2: BaseResp base_resp
 }
 
-struct CheckUserRequest {
-    1: string username (vt.min_size = "1")
-    2: string password (vt.min_size = "1")
+struct CheckUserRequest{
+1: string username (vt.min_size = "1")
+2: string password (vt.min_size = "1")
 }
 
-struct CheckUserResponse {
-    1: i64 user_id
-    2: BaseResp base_resp
+struct  CheckUserResponse{
+1:  i64 user_id
+2:  BaseResp base_resp
 }
 
-service UserService {
-    CreateUserResponse CreateUser(1: CreateUserRequest req)
-    MGetUserResponse MGetUser(1: MGetUserRequest req)
-    CheckUserResponse CheckUser(1: CheckUserRequest req)
+
+service UserService{
+CreateUserResponse CreateUser (1: CreateUserRequest req)
+MGetUserResponse MGetUser(1: CheckUserRequest req)
+CheckUserResponse CheckUser(1 : CheckUserRequest req)
 }
